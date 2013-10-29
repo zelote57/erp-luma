1bf00a6103772babc46c0077b58c6f579c5683f4
lemuelaceron@yahoo.com <lemuelaceron@yahoo.com@b7d9f923-77a2-037a-aecf-4b7faa953cc7>
2013-10-29 13:25:49 +0000

    Include handling of unhandled error.

--- RetailPlus/trunk/RetailPlusClient/Program.cs
+++ RetailPlus/trunk/RetailPlusClient/Program.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Diagnostics;
 using System.Management;
 using System.Windows.Forms;
+using System.Threading;
 using System.Xml;
 using AceSoft.RetailPlus.Client.UI;
 
@@ -10,9 +12,90 @@ namespace AceSoft.RetailPlus.Client
 	{
 		#region Application Main
 
+        // Handle the UI exceptions by showing a dialog box, and asking the user whether 
+        // or not they wish to abort execution. 
+        private static void MainWnd_UIThreadException(object sender, ThreadExceptionEventArgs t)
+        {
+            DialogResult result = DialogResult.Cancel;
+            try
+            {
+                Event clsEvent = new Event();
+                clsEvent.AddErrorEventLn(t.Exception);
+
+                result = ShowThreadExceptionDialog("Windows Forms Error", t.Exception);
+            }
+            catch
+            {
+                try
+                {
+                    MessageBox.Show("Fatal Windows Forms Error", "Fatal Windows Forms Error", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+
+            // Exits the program when the user clicks Abort. 
+            if (result == DialogResult.Abort)
+                Application.Exit();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+                
+                Event clsEvent = new Event();
+                clsEvent.AddErrorEventLn(ex);
+
+                string errorMsg = "RetailPlus FE Error. Please contact the adminstrator " +
+                    "with the following information:\n\n";
+
+                // Since we can't prevent the app from terminating, log this to the event log. 
+                if (!EventLog.SourceExists("ThreadException"))
+                {
+                    EventLog.CreateEventSource("ThreadException", "RetailPlus");
+                }
+
+                // Create an EventLog instance and assign its source.
+                EventLog myLog = new EventLog();
+                myLog.Source = "ThreadException";
+                myLog.WriteEntry(errorMsg + ex.Message + "\n\nStack Trace:\n" + ex.StackTrace);
+                
+            }
+            catch (Exception exc)
+            {
+                try
+                {
+                    MessageBox.Show("Fatal Non-UI Error",
+                        "Fatal Non-UI Error. Could not write the error to the event log. Reason: "
+                        + exc.Message, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        // Creates the error message and displays it. 
+        private static DialogResult ShowThreadExceptionDialog(string title, Exception e)
+        {
+            string errorMsg = "An application error occurred. Please contact the adminstrator " +
+                "with the following information:\n\n";
+            errorMsg = errorMsg + e.Message + "\n\nStack Trace:\n" + e.StackTrace;
+            return MessageBox.Show(errorMsg, title, MessageBoxButtons.AbortRetryIgnore,
+                MessageBoxIcon.Stop);
+        }
+
 		[STAThread]
 		static void Main() 
 		{
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            System.Windows.Forms.Application.ThreadException += new ThreadExceptionEventHandler(MainWnd_UIThreadException);
+
 			Event clsEvent = new Event();
 			clsEvent.AddEvent("starting.... loading splash screen...");
 			SplashWnd appsplash = new SplashWnd();

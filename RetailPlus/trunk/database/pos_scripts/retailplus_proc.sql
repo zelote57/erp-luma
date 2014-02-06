be4f22961eaa5fbfa78c9aa5ead99d34027fc383
lemuelaceron <lemuelaceron@yahoo.com>
2014-02-06 14:19:54 +0000

    include
    procTerminalReportRePrintedUpdate

--- RetailPlus/trunk/database/pos_scripts/retailplus_proc.sql
+++ RetailPlus/trunk/database/pos_scripts/retailplus_proc.sql
@@ -1079,7 +1079,8 @@ BEGIN
 					NoOfVoidTransactions, NoOfRewardPointsPayment, NoOfTotalTransactions, 
 					DateLastInitialized, TrustFund, 
 					NoOfDiscountedTransactions, NegativeAdjustments, NoOfNegativeAdjustmentTransactions,
-					PromotionalItems, CreditSalesTax, BatchCounter, InitializedBy) 
+					PromotionalItems, CreditSalesTax, BatchCounter, 
+					NoOfReprintedTransaction, TotalReprintedTransaction, InitializedBy) 
 				(SELECT 
 					BranchID, TerminalID, TerminalNo, BeginningTransactionNo, EndingTransactionNo, ZReadCount, 
 					XReadCount, GrossSales, TotalDiscount, TotalCharge, DailySales, 
@@ -1098,7 +1099,8 @@ BEGIN
 					NoOfVoidTransactions, NoOfRewardPointsPayment, NoOfTotalTransactions, 
 					DateLastInitialized, decTrustFund,
 					NoOfDiscountedTransactions, NegativeAdjustments, NoOfNegativeAdjustmentTransactions,
-					PromotionalItems, CreditSalesTax, BatchCounter, strInitializedBy FROM tblTerminalReport WHERE BranchID = intBranchID AND TerminalNo = strTerminalNo);
+					PromotionalItems, CreditSalesTax, BatchCounter, 
+					NoOfReprintedTransaction, TotalReprintedTransaction, strInitializedBy FROM tblTerminalReport WHERE BranchID = intBranchID AND TerminalNo = strTerminalNo);
 					
 	UPDATE tblTerminalReport SET 
 		OldGrandTotal =  decNewGrandTotal / ((100 - decVirtualTrustFund) / 100),
@@ -1170,6 +1172,8 @@ BEGIN
 					PromotionalItems					=  0,
 					CreditSalesTax						=  0,
 					BatchCounter						=  1,
+					NoOfReprintedTransaction			=  0,
+					TotalReprintedTransaction			=  0, 
 					DateLastInitialized					=  dteDateLastInitialized
 			WHERE BranchID = intBranchID AND TerminalNo = strTerminalNo;
 			
@@ -6984,16 +6988,16 @@ create procedure procSalutationSelect(
 			IN SortOrder varchar(4))
 BEGIN
 	SET @SQL = CONCAT('	SELECT 
-							 ConfigValue SalutationCode
-							,ConfigName SalutationName
-						FROM sysConfig cnfg
-						WHERE Category = ''SALUTATION'' ');
+							 SalutationCode
+							,SalutationName
+						FROM tblSalutations cnfg
+						WHERE 1=1 ');
 	
 	IF IFNULL(Salutation,'') <> '' THEN
-		SET @SQL = CONCAT(@SQL, 'AND ConfigName LIKE ''%',Salutation,'%'' ');
+		SET @SQL = CONCAT(@SQL, 'AND SalutationCode LIKE ''%',Salutation,'%'' ');
 	END IF;
 
-	SET @SQL = CONCAT(@SQL, 'ORDER BY ',IF(IFNULL(SortField,'')='','cnfg.ConfigValue',SortField),' ',IFNULL(SortOrder,'ASC'),' ');
+	SET @SQL = CONCAT(@SQL, 'ORDER BY ',IF(IFNULL(SortField,'')='','cnfg.SalutationCode',SortField),' ',IFNULL(SortOrder,'ASC'),' ');
 
 	PREPARE cmd FROM @SQL;
 	EXECUTE cmd;
@@ -7229,7 +7233,7 @@ delimiter ;
 	
 	Desc: This will get the all information of a contact
 
-	CALL procContactSelect(1, 0, null, null, null, null, null, 0, 0, '1900-01-01', '1900-01-01', '1900-01-01', '1900-01-01', 0, 0, 0, 'ContactID','');
+	CALL procContactSelect(1, 0, null, null, null, null, null, 0, 1, '1900-01-01', '1900-01-01', '1900-01-01', '1900-01-01', 0, 0, 0, 'ContactID','');
 	
 **************************************************************/
 delimiter GO
@@ -7313,12 +7317,12 @@ BEGIN
 
 	-- Added 10Oct2013 
 	IF IFNULL(BirthMonth,0) <> 0 THEN
-		SET @SQL = CONCAT(@SQL, 'AND (DATE_FORMAT(IFNULL(IFNULL(addon.BirthDate,rwrd.BirthDate),0),''%m'') = ''',BirthMonth,''' ');
-		SET @SQL = CONCAT(@SQL, '	  OR DATE_FORMAT(IFNULL(addon.SpouseBirthDate,0),''%m'') = ''',BirthMonth,''') ');
+		SET @SQL = CONCAT(@SQL, 'AND (DATE_FORMAT(IFNULL(IFNULL(addon.BirthDate,rwrd.BirthDate),0),''%m'') = ',BirthMonth,' ');
+		SET @SQL = CONCAT(@SQL, '	  OR DATE_FORMAT(IFNULL(addon.SpouseBirthDate,0),''%m'') = ',BirthMonth,') ');
 	END IF;
 
 	IF IFNULL(AnniversaryMonth,0) <> 0 THEN
-		SET @SQL = CONCAT(@SQL, 'AND DATE_FORMAT(IFNULL(addon.AnniversaryDate,0),''%m'') = ''',AnniversaryMonth,''' ');
+		SET @SQL = CONCAT(@SQL, 'AND DATE_FORMAT(IFNULL(addon.AnniversaryDate,0),''%m'') = ',AnniversaryMonth,' ');
 	END IF;
 
 	
@@ -7348,7 +7352,6 @@ BEGIN
 	SET @SQL = CONCAT(@SQL, 'ORDER BY ',IF(IFNULL(SortField,'')='','ContactCode, ContactName, LastName',SortField),' ',IFNULL(SortOrder,'ASC'),' ');
 
 	SET @SQL = CONCAT(@SQL,IF(lngLimit=0,'',CONCAT('LIMIT ',lngLimit,' ')));
-
 
 	PREPARE cmd FROM @SQL;
 	EXECUTE cmd;
@@ -7653,4 +7656,32 @@ ALTER DATABASE pos CHARACTER SET utf8;
 
 SELECT default_character_set_name FROM information_schema.SCHEMATA S WHERE schema_name = 'pos';
 
-update tblproducts set SupplierID = 2 where supplierid not in (select distinct contactid from tblcontacts);
+-- default the supplier to RetailPlus Default Supplier for those that doesnt have supplierid
+UPDATE tblProducts SET SupplierID = 2 WHERE SupplierID NOT IN (SELECT DISTINCT contactid from tblContacts);
+
+/*********************************
+	procTerminalReportRePrintedUpdate
+	Lemuel E. Aceron
+	
+	[02/06/2014]  - create this procedure
+	
+	CALL procTerminalReportRePrintedUpdate(1, '01', '"00000000153656');
+
+*********************************/
+DROP PROCEDURE IF EXISTS procTerminalReportRePrintedUpdate;
+delimiter GO
+
+create procedure procTerminalReportRePrintedUpdate(IN intBranchID int(4), IN strTerminalNo VARCHAR(30), IN strTransactionNo VARCHAR(30))
+BEGIN
+	DECLARE decSubTotal DECIMAL;
+
+	SET decSubTotal = (SELECT SUM(SubTotal) FROM tblTransactions WHERE TransactionNo = strTransactionNo AND TerminalNo = strTerminalNo);
+
+	UPDATE tblTerminalReport SET 
+		NoOfReprintedTransaction = NoOfReprintedTransaction + 1,
+		TotalReprintedTransaction = TotalReprintedTransaction + IFNULL(decSubTotal,0)
+	WHERE BranchID = intBranchID AND TerminalNo = strTerminalNo;
+	
+END;
+GO
+delimiter ;
